aea492972394cdfaa7ad27987783ff23887c3f1b
muhammedirfan <irfannadapuram>
2023-01-08 23:52:52 +0530

    navbar banner about others adding started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Footer from './components/Footer/Footer.js';
+import Navbar from './components/Navbar/Navbar.js';
+import Events from './components/Events/Events'
+import Banner from './components/Banner/Banner'
+import About from './components/About/About';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-       
+        
+       <Navbar/>
+       <Banner/>
+       <About/>
+       <Events/>
+       <Footer/>
        
       </header>
     </div>
