bdc5a97f956a27e2febd6d71ce24735055a8c42f
muhammedirfan <irfannadapuram>
2023-01-16 18:39:12 +0530

    navbar button added, background click when nav cliked

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,7 @@ import "./Navbar.css"
 
 function Navbar() {
   const [moved, setMoved] = useState(false)
+  const [menu, setMenu] = useState(false)
   const scrolled = (h) => {
     if (window.scrollY >= 50) {
       setMoved(true)
@@ -11,19 +12,25 @@ function Navbar() {
     else {
       setMoved(false)
     }
+    setMenu(false)
   }
   window.addEventListener("scroll", scrolled)
   return (
-    <div className={moved ? "navbar scrolled" : "navbar"}>
+    <div className="navbar ">
+      <div className={moved ? "scrolled blur" :"scrolled"}></div>
       <h2 className="logo">IEDC</h2>
-      <div className='navShort'>
+      <div className={menu ? "navLinks" : "navLinks hide"}>
         <h4>Events</h4>
         <h4>Team</h4>
         <h4>About</h4>
         <h4>Contact</h4>
 
-
       </div>
+      <label className='menuBtn' onClick={() => {
+        setMenu(!menu);
+        console.log("clicked");
+      }}><span></span></label>
+      <div className={menu? 'curtain ': "curtain none"} onClick={()=>{setMenu(false)}}></div>
 
     </div>
   )
