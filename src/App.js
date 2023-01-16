e915a933684b95fbb07535d0bea5d1222bdcebb5
muhammedirfan <irfannadapuram>
2023-01-16 13:53:11 +0530

    fixed screen width 280px, media lanscape fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Footer from './components/Footer/Footer.js';
 import Navbar from './components/Navbar/Navbar.js';
-import Events from './components/Events/Events'
+// import Events from './components/Events/Events'
 import Banner from './components/Banner/Banner'
 import About from './components/About/About';
 import ImageSlider from './components/EventsCopy/Events';
--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,36 +1,36 @@
-import './Events.css';
-import React from 'react';
+// import './Events.css';
+// import React from 'react';
 
-function Events() {
-  const images = [
-    "http://iedc.gectcr.ac.in/images/img-1.jpeg",
-    "http://iedc.gectcr.ac.in/images/img-2.jpeg",
-    "http://iedc.gectcr.ac.in/images/img-1.jpeg",
-    "http://iedc.gectcr.ac.in/images/img-2.jpeg",
-    "http://iedc.gectcr.ac.in/images/img-1.jpeg"
-  ]
-  // const order = 1;
-  return (
-    <div className='events'>
-      <div className='images'>
-        {images.map((obj) => {
-        return (
-            <div className="image">
-              <img src={obj} alt="NETWORK ERROR" draggable="false" />
-            </div>
-          )
-        })}
-      </div>
-      <div className="radio">
+// function Events() {
+//   const images = [
+//     "http://iedc.gectcr.ac.in/images/img-1.jpeg",
+//     "http://iedc.gectcr.ac.in/images/img-2.jpeg",
+//     "http://iedc.gectcr.ac.in/images/img-1.jpeg",
+//     "http://iedc.gectcr.ac.in/images/img-2.jpeg",
+//     "http://iedc.gectcr.ac.in/images/img-1.jpeg"
+//   ]
+//   // const order = 1;
+//   return (
+//     <div className='events'>
+//       <div className='images'>
+//         {images.map((obj) => {
+//         return (
+//             <div className="image">
+//               <img src={obj} alt="NETWORK ERROR" draggable="false" />
+//             </div>
+//           )
+//         })}
+//       </div>
+//       <div className="radio">
 
-        <input type="radio" name='img' />
-        <input type="radio" name='img' />
-        <input type="radio" name='img' />
-        <input type="radio" name='img' />
-        <input type="radio" name='img' />
-      </div>
-    </div>
-  )
-}
+//         <input type="radio" name='img' />
+//         <input type="radio" name='img' />
+//         <input type="radio" name='img' />
+//         <input type="radio" name='img' />
+//         <input type="radio" name='img' />
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Events
+// export default Events
