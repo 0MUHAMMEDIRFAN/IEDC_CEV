d816a168a7afdc3089761f4720bd45fe4a6c0cae
muhammedirfan <irfannadapuram>
2023-02-08 20:36:34 +0530

    made image slider. completed partially. need to complete radio buttons

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,5 +1,6 @@
 import './Events.css';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 function Events() {
   const images = [
@@ -10,11 +11,34 @@ function Events() {
     "https://picsum.photos/id/4/5000/3333",
     "https://picsum.photos/id/5/5000/3333"
   ]
+  const [scrolled, setScrolled] = useState(0)
+  const [imgwidth, setImgwidth] = useState(3)
+  const [getId, setGetId] = useState(0)
+  // const sectionwidth = window.innerWidth;
+  let radio = document.querySelector("#id" + getId)
+  useEffect(() => {
+    setImgwidth(document.querySelector(".events .images .image").offsetWidth);
+    
+  }, [])
+  const slide = (event) => {
+    setScrolled(event.target.scrollLeft);
+    setGetId(Math.round((scrolled) / (imgwidth)))
+    radio.checked = true;
+    // console.log(Math.round((scrolled) / (imgwidth)));
+    scroll(event)
+  }
+  const scroll = (event) => {
+    // setScrolled(120)
+    // event.target.scrollLeft = 0;
+
+  }
+
+
   // const order = 1;
   return (
-    <div className='events'>
-      
-      <div className='images'>
+    <div className='events' id='events' >
+
+      <div className='images' id="images" onScroll={slide}>
         {images.map((obj) => {
           return (
             <div className="image">
@@ -24,15 +48,24 @@ function Events() {
         })}
       </div>
       <div className="radio">
+        {
+          images.map((key, index) => {
+            return (
+              <input type="radio" name="img" id={"id" + index} onClick={scroll} />
+            )
 
-        <input type="radio" defaultChecked name='img' />
+          })
+        }
+        {/* <input type="radio" name='img' defaultChecked />
         <input type="radio" name='img' />
         <input type="radio" name='img' />
         <input type="radio" name='img' />
         <input type="radio" name='img' />
+      <input type="radio" name='img' /> */}
       </div>
     </div>
   )
+
 }
 
 export default Events
