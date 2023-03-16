05c034e7e843b683f4708abfeea28e7171a7b589
muhammedirfan <irfannadapuram>
2023-03-17 00:06:54 +0530

    map made to responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,42 @@
 import React from 'react'
 import './Footer.css'
+import mapImage from "../../images/map.png"
 
 function Footer() {
   return (
     <div className='footer'>
-      <h2>Follow on</h2>
-      <div className='social'>
-        <i className='bx bxl-facebook'></i>
-        <i className='bx bxl-linkedin'></i>
-        <i className='bx bxl-twitter' ></i>
-        <i className='bx bxl-instagram' ></i>
+      <div className="mapContainer">
+        <img src={mapImage} alt="Error" />
+        <div className="details">
+          <h2>Location & Contact</h2>
+          <div className="location">
+            <i class='bx bx-map'></i>
+            <span>IEDC Collage of Engineering<br />Vadakara<br />kurunthodi,cherandathoor<br /><b>234423,lafksd</b></span>
+          </div>
+          <div className="contact">
+            <div className="phone">
+              <i class='bx bxs-phone' ></i>
+              <span>+910000000000</span>
+            </div>
+            <div className="phone">
+              <i class='bx bx-envelope' ></i>
+              <span>abcd@xyz.com</span>
+            </div>
+            <div className='social'>
+              <i className='bx bxl-facebook'></i>
+              <i className='bx bxl-linkedin'></i>
+              <i className='bx bxl-twitter' ></i>
+              <i className='bx bxl-instagram' ></i>
+            </div>
+          </div>
+        </div>
+
       </div>
+      {/* <div className='footerDetails'> */}
+      {/* <h4>Follow on</h4> */}
+
       <h4>©IEDC CEV - All rights reserved</h4>
+      {/* </div> */}
     </div>
   )
 }
