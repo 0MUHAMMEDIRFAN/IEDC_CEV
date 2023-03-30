afe0bd3a7a2924fdee8966bc001edd6cad308934
muhammedirfan <irfannadapuram>
2023-03-30 15:34:17 +0530

    bug fixes, navbar blur on safari demo delted on startup

--- src/components/Startup/Startup.js
+++ src/components/Startup/Startup.js
@@ -1,34 +1,10 @@
 import React from 'react'
-import {Link} from "react-router-dom" 
+import { Link } from "react-router-dom"
 import FI from "../../images/FI.png"
 import "./Startup.css"
 
 function Startup(props) {
     const startups = [
-        {
-            Name: "FINITE INTELLIGENCE",
-            Logo: FI,
-            Story: "Finite Intelligence is a technology popularization start up initiative at College of Engineering Vadakara under Innovation and Entrepreneurship Development Cell (IEDC) supported by Kerala Startup Mission (KSUM). This startup initiative shaped as a successful outcome of e Yantra LabSetup Initiative (e-LSI) by IIT Bombay. Primary focus of Finite Intelligence is to conduct technology outreach programs for students with a mission of ‘learn by doing’. Students can harness the hands-on experience of Industry 4.0 technologies such as Artificial Intelligence, Internet of Things, Robotics through training programs and live projects. We train students, teachers and institutions by establishing an innovation culture tasked with solving real problems using technology.",
-            Vision: "To enable students to attain unprivileged access to Industry 4.0 technologies and to ensure early roll on to skill aligned learning. ",
-            Mission: "",
-            Do: ""
-        },
-        {
-            Name: "FINITE INTELLIGENCE",
-            Logo: FI,
-            Story: "Finite Intelligence is a technology popularization start up initiative at College of Engineering Vadakara under Innovation and Entrepreneurship Development Cell (IEDC) supported by Kerala Startup Mission (KSUM). This startup initiative shaped as a successful outcome of e Yantra LabSetup Initiative (e-LSI) by IIT Bombay. Primary focus of Finite Intelligence is to conduct technology outreach programs for students with a mission of ‘learn by doing’. Students can harness the hands-on experience of Industry 4.0 technologies such as Artificial Intelligence, Internet of Things, Robotics through training programs and live projects. We train students, teachers and institutions by establishing an innovation culture tasked with solving real problems using technology.",
-            Vision: "To enable students to attain unprivileged access to Industry 4.0 technologies and to ensure early roll on to skill aligned learning. ",
-            Mission: "",
-            Do: ""
-        },
-        {
-            Name: "FINITE INTELLIGENCE",
-            Logo: FI,
-            Story: "Finite Intelligence is a technology popularization start up initiative at College of Engineering Vadakara under Innovation and Entrepreneurship Development Cell (IEDC) supported by Kerala Startup Mission (KSUM). This startup initiative shaped as a successful outcome of e Yantra LabSetup Initiative (e-LSI) by IIT Bombay. Primary focus of Finite Intelligence is to conduct technology outreach programs for students with a mission of ‘learn by doing’. Students can harness the hands-on experience of Industry 4.0 technologies such as Artificial Intelligence, Internet of Things, Robotics through training programs and live projects. We train students, teachers and institutions by establishing an innovation culture tasked with solving real problems using technology.",
-            Vision: "To enable students to attain unprivileged access to Industry 4.0 technologies and to ensure early roll on to skill aligned learning. ",
-            Mission: "",
-            Do: ""
-        },
         {
             Name: "FINITE INTELLIGENCE",
             Logo: FI,
@@ -48,7 +24,7 @@ function Startup(props) {
             </div>
             <div className="startups">
                 {startups.map((obj, index) => {
-                    if(index>=props.num)return(null)
+                    if (index >= props.num) return (null)
                     return (
                         <div className='contents' key={index}>
                             <div className="logo">
