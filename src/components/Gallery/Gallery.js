54dc93e69bc321896d37cacc0deba79bedd2782b
muhammedirfan <irfannadapuram>
2023-03-31 11:24:15 +0530

    bug fixed, images folder deletion

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -9,14 +9,7 @@ function Gallery(props) {
     return r.keys().map(r);
   }
   const images = importAll(require.context('./Images', false, /\.(png|jpe?g|svg)$/));
-  // const images = [
-  //   "https://picsum.photos/id/0/5000/3333",
-  //   "https://picsum.photos/id/1/5000/3333",
-  //   "https://picsum.photos/id/2/5000/3333",
-  //   "https://picsum.photos/id/3/5000/3333",
-  //   "https://picsum.photos/id/4/5000/3333",
-  //   "https://picsum.photos/id/5/5000/3333"
-  // ]
+  
   const span = (index) => {
     const img = document.getElementById(`img${index}`);
     if(document.getElementById(`img${index}`) && Math.round(img.offsetHeight / 150) >= 2 ){
